b0cf5b46197767fd4490ea4a930ecb60a5c3ca1b
miguelfiais <miguel.fiais10@gmail.com>
2023-08-31 10:45:40 -0300

    fix: change button background with active state

--- src/app/repository/components/Button.tsx
+++ src/app/repository/components/Button.tsx
@@ -8,12 +8,9 @@ const Button = ({ active, ...props }: ButtonProps) => {
   return (
     <button
       {...props}
-      className={`bg-gray-900 text-white rounded py-1 px-2 text-sm disabled:cursor-not-allowed disabled:opacity-50 ${
-        active && 'bg-blue-600'
-      }`}
-    >
-      {props.children}
-    </button>
+      className={` text-white rounded py-1 px-2 text-sm disabled:cursor-not-allowed disabled:opacity-50 ${
+        active ? 'bg-blue-600' : "bg-gray-900"   
+      }`}/>
   )
 }
 
